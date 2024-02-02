56cea13390093ccc5c9f45f04861e2c4eceb78c4
Ivan Murashov <smile709@ya.com>
2024-02-02 21:40:02 +0300

    Comlete the Programm

---
+++ src/ToyShop.java
@@ -0,0 +1,57 @@
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.PriorityQueue;
+import java.util.Random;
+
+public class ToyShop {
+    private class Toy {
+        public int id;
+        public String name;
+        public int weight;
+
+        public Toy(int id, String name, int weight) {
+            this.id = id;
+            this.name = name;
+            this.weight = weight;
+        }
+    }
+
+    private PriorityQueue<ToyShop.Toy> toysQueue = new PriorityQueue<>((t1, t2) -> Integer.compare(t2.weight, t1.weight));
+
+    public void put(String input) {
+        String[] parts = input.split(" ");
+        int id = Integer.parseInt(parts[0]);
+        String name = parts[1];
+        int weight = Integer.parseInt(parts[2]);
+        Toy toy = new Toy(id, name, weight);
+        toysQueue.add(toy);
+    }
+
+    public int get() {
+        int totalWeight = toysQueue.stream().mapToInt(t -> t.weight).sum();
+        Random random = new Random();
+        int randomNumber = random.nextInt(totalWeight);
+        int cumulativeWeight = 0;
+        for (Toy toy : toysQueue) {
+            cumulativeWeight += toy.weight;
+            if (randomNumber < cumulativeWeight) {
+                return toy.id;
+            }
+        }
+        return -1;
+    }
+
+    public void writeResultsToFile(String filename, int count) {
+        try {
+            FileWriter writer = new FileWriter(filename);
+            for (int i = 0; i < count; i++) {
+                int result = get();
+                writer.write(Integer.toString(result));
+                writer.write("\n");
+            }
+            writer.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
